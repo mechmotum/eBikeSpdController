8d5ac3e48adfa570329cb805d902e917446e9f31
tzmetz <39064495+tzmetz@users.noreply.github.com>
2019-10-12 10:54:47 -0700

    Updated documentation on all design codes

--- Design/ControllerTuning.m
+++ Design/ControllerTuning.m
@@ -3,13 +3,18 @@
 %%%% -- This is the second code used in the controller design process -- %%%%
 
 %%% DESCRIPTION
-%%% This code uses the Control System Toolbox to tune a PID controller to acheive zero steady error 
-%%%  and reasonable transient behavior for a unity feedback control architecture 
+%%% This code uses the Control System Toolbox to tune a PID controller given the plant model identified in the SystemID.m code. 
+%%% Goals of the controller are to acheive zero steady error and a transient behavior matching that which was measured on the actual bike.
+%%% Finally, the mechanical power associated with the closed loop response
+%%% is calculated to ensure excessive power is not required 
 
 %%%%%% Doing the all clear %%%%%%
 clear
 clc
 close all
+
+%% Acquiring Settling Time of The Measured Time Response 
+% Here we measure the settling time of the measured ebike time response to use it to help us match the closed loop system's transient response with the measured one 
 
 % Loading Measured Time Response Data
 load('00912.mat') 
@@ -29,6 +34,7 @@ spdFinal = mean(ydata(2218:end)); % First index found from looking at the plot a
 msrdSetTime = xdata(2218);
 fprintf('Measured Settling Time = %.1f (s)\n',msrdSetTime)
 
+%%
 %%%%%% PLANT MODEL %%%%%% (Identified from the system identification step)
 %%%
 %%% v(s)/V(s) = 15.32 / (s^2 + 70.81*s + 7.34 ) 
@@ -41,35 +47,32 @@ Gp = tf(15.32,[1,70.81,7.34]);
 %%%%%% Using the Control System Toolbox to tune a PID controller %%%%%% 
 % INSTRUCTIONS
 % Uncomment either of the following lines of code to launch either app for tuning a PID controller
+% Please see MATLAB's documentation for either app for help with using the
+% app
 
- %pidTuner(Gp,'pid')
-% controlSystemDesigner('rlocus',Gp)
+% Uncomment either one of these lines of code to use the app
+%pidTuner(Gp,'pid')
+%controlSystemDesigner('rlocus',Gp)
 
 %%%%%% Declaring the Control Architecture %%%%%%
-% Gc = pid(4.18,0.884,0); % PID constants found from using controller tuning app
-% Gc = pid(0.363,0.0955,0); % PID constants found from using controller tuning app
-% Gc = pid(0.74,0.0908,0); % PID constants found from using controller tuning app
-% Gc = pid(1.01,0.108,0); % PID constants found from using controller tuning app
-% Gc = pid(1.15,0.141,0); % PID constants found from using controller tuning app
-% Gc = pid(1.12,0.147,0); % PID constants found from using controller
-% tuning app 
 Gc = pid(1.03,0.145,0); % PID constants found from using controller tuning app
-sys_cl = feedback(Gc*Gp,1);
+sys_cl = feedback(Gc*Gp,1); % Declaring a unity feedback control architecture with the plant model Gp and PID Controller Gc
 
 % Simulating the Closed Loop Step Response
 t = linspace(0,20,8000); 
 opt = stepDataOptions('StepAmplitude',spdFinal);
 y = step(sys_cl,t,opt);  
 
+% Plotting Measured vs Closed Loop Time Responses
 figure('Name','Comparing Measured to Tuned Performance')
 plot(t,y,'b') 
 hold on 
 plot(xdata,ydata,'r')
-title('Comparison Plot Between Measured and Tuned Time Response')
+title('Comparison Plot Between Measured and Closed Loop Time Responses')
 xlabel('Time [s]')
 ylabel('Speed [m/s]') 
 
-% Gathering and Plotting Additional Time Responses At Different Magnitudes 
+%% Gathering and Plotting Additional Time Responses At Different Step Amplitudes
 opt1 = stepDataOptions('StepAmplitude',5);
 opt2 = stepDataOptions('StepAmplitude',3);
 opt3 = stepDataOptions('StepAmplitude',1); 
@@ -84,13 +87,13 @@ plot(t,y2,'g')
 plot(t,y3,'k') 
 legend('Setpt = 7.68','5','3','1')
 
-% Measuring the Performance of the Closed Loop System
+%% Measuring the Performance of the Closed Loop System
 F = stepinfo(sys_cl);
 fprintf("Closed loop settling time: %.2f (s) \n", F.SettlingTime)
-fprintf("Closed loop overshoot percentage: %.2f (precent) \n", F.Overshoot) 
+fprintf("Closed loop overshoot percentage: %.2f (percent) \n", F.Overshoot) 
 
-% Calculating Mechanical Power  
-v_t = y; % substituting y (independent variable from step response called above) for v(t)
+%% Calculating eBike Mechanical Power During Closed Loop Time Response  
+v_t = y; % substituting y for v(t)
 
 % Check Units on Aerodynamic Drag Force
 power = v_t.*(0.368.*v_t.^2 + 0.008*95.05*9.8); % Power [W] calculated by multiplying ebike velocity by the 
@@ -109,6 +112,7 @@ title('Ebike Mechanical Power During Step Response')
 yyaxis left, ylabel('Power [W]')
 yyaxis right, ylabel('Velocity [m/s]')
 xlabel('Time [s]')
-legend('Power','Velocity')
+legend('Power','Velocity') 
 
-
+% Documentation on the power rating of our motor is not available, so assuming it is on the low end of commercial ebike hub motors (250W) is a safe assumption. 
+% Based on this assumption and the plot above, the tuned controller will not require more power from the eBike powertrain than what is available.
--- Design/SystemID.m
+++ Design/SystemID.m
@@ -4,8 +4,9 @@
 
 %%% DESCRIPTION
 %%% This code identifies the constants of the derived ebike plant model using a least-sqaures curve fitting method
-%%%  based on initial guesses of the plant model constants 
-%%%   This code requires 00912.mat data file in order to run
+%%%  based on initial guesses of the plant model constants. 
+%%%   Please read this blog post to learn more about the derived ebike plant model: https://mechmotum.github.io/blog/ebike-controller-design.html  
+%%%    This code requires 00912.mat data file in order to run
 
 %%%%%% Doing the all clear %%%%%%
 clear
@@ -20,46 +21,20 @@ close all
 
 % Initializing Plant Model Constants  
 % These constants are initial guesses
-Ka = 0.368;      % Aerodynamic Drag coefficient taken from Bicycling Science 3rd Edition
+Ka = 0.368;      % Aerodynamic Drag coefficient taken from Bicycling Science 3rd Edition by David Gordon Wilson
 kt = 0.002;      % Motor torque constant for a 500 Kv motor
 Rm = 0.15;       % [ohms] Motor resistance taken as an average from other similar motors
 m = 95.05;       % [kg] Mass of bike+rider from Modeling the Manually Controlled Bicycle
-J = 2*0.15;      % [kg*m^2] From Modeling the Manually Controlled Bicycle (multiplied by two to lump both front and rear wheels)   
+J = 2*0.15;      % [kg*m^2] From Modeling the Manually Controlled Bicycle by Hess et. al. (multiplied by two to lump both front and rear wheels)   
 L = 0.06;        % [H] Motor inductance taken as an average from other similar motors
 Rw = 0.34;       % [m] Radius of the rear wheel
-
-% Loading Measured Time Response Data 
-cd('C:\Users\1tzm_000\Documents\Research\Testing\Dry Testing\Raw Data') 
-
-rawData = readmatrix('TEST_85.txt'); 
-ccState = rawData(:,1);
-setpoint = rawData(:,2);
-Tsig = rawData(:,3);
-input= rawData(:,4); % m/s
-genVolt= rawData(:,5); % V
-output= rawData(:,6); 
-time = rawData(:,7); 
-loopStartState = rawData(:,8);
-ifState1 = rawData(:,9);
-ifState2 = rawData(:,10);
-ifState3 = rawData(:,11);
-ifState4 = rawData(:,12);
-TsigAnalogRead = rawData(:,13); 
-unfiltrdGenVoltage = rawData(:,13);
-cd('C:\Users\1tzm_000\Documents\Research\SourceCode\Design') 
-xdata_1 = transpose(time(1207:1635)); % indices found from plot of time response
-ydata_1 = transpose(input(1207:1635));  
-cd('C:\Users\1tzm_000\Documents\Research\SourceCode\Design') 
 
 % Loading Measured Time Response Data
 load('00912.mat') 
 xdata = linspace(0,20,2401);  
 ydata = (2*pi()/60)*(456.3862.*NIData(300:2700,7)-1.2846)*(0.341*0.028985/0.333375); %the raw data from 00912.mat must first be processed to convert it to m/s (see http://moorepants.github.io/dissertation/davisbicycle.html#calibration) 
 ydata = transpose(ydata); 
-stepVoltage = 4.27; % [V] hall effect sensor voltage measured with a multimeter at full throttle
-
-%xdata = xdata_1;
-%ydata = ydata_1;
+stepVoltage = 4.27; % [V] hall effect sensor voltage measured with a multimeter while at full throttle
 
 % Declaring Coefficeints of Simplified Plant Model 
 %-          Gp = e / (s^2 + f*s + g)            -%
@@ -73,8 +48,7 @@ g = c/a;
 
 % Function that computes the time response of the transfer function. 
 % This function is used in the least-squares curve fitting
-% The expression below is the inverse laplace of (stepVoltage/s) * (e / s^2 + f*s + g) where x is an array containing the values of coefficients e, f and g
-%time_response = @(x,xdata) 8*x(1)/x(3) - (8*x(1)*exp(-(x(2).*xdata)/2).*(cosh(xdata.*((x(2)^2)/4 - x(3))^(1/2)) + (x(2)*sinh(xdata.*((x(2)^2)/4 - x(3))^(1/2)))/(2*((x(2)^2)/4 - x(3))^(1/2))))/x(3); 
+% The expression below is the inverse laplace of (stepVoltage/s) * (e / s^2 + f*s + g). Below, x is an array containing the values of coefficients e, f and g
 time_response = @(x,xdata) (427*x(1))/(100*x(3)) - (427*x(1)*exp(-(x(2).*xdata)/2).*(cosh(xdata.*(x(2)^2/4 - x(3))^(1/2)) + (x(2)*sinh(xdata.*(x(2)^2/4 - x(3))^(1/2)))/(2*(x(2)^2/4 - x(3))^(1/2))))/(100*x(3));
 
 % Initial Guess
@@ -111,45 +85,50 @@ set(h, ...
 Gp = tf(x(1),[1,x(2),x(3)]);
 fprintf('The identified plant model is:'); display(Gp)
 
-%% Generating Plot for Paper 
-figure();
+%% Generating Plot for Publishing
 
-pos = get(gcf, 'Position');
-width = 7; % width in inches 
-height = 6; % height in inches
-set(gcf, 'Position', [pos(1) pos(2) width*100 height*100])
+publishing = 0; % For publishing set this to 1
 
-plot(xdata,ydata,'r',t_2,time_response(x,t_2),'b', 'LineWidth', 2 )  
+if publishing 
+    figure();
 
-% Gathering and Plotting Additional Time Responses At Different Magnitudes 
-%opt1 = stepDataOptions('StepAmplitude',5);
-opt2 = stepDataOptions('StepAmplitude',3);
-opt3 = stepDataOptions('StepAmplitude',1); 
+    pos = get(gcf, 'Position');
+    width = 7; % width in inches 
+    height = 6; % height in inches
+    set(gcf, 'Position', [pos(1) pos(2) width*100 height*100])
 
-%y1 = step(Gp,t_2,opt1);
-y2 = step(Gp,t_2,opt2);
-y3 = step(Gp,t_2,opt3);
+    plot(xdata,ydata,'r',t_2,time_response(x,t_2),'b', 'LineWidth', 2 )  
 
-hold on  
-%plot(t_2,y1,'b')
-plot(t_2,y2,'g')
-plot(t_2,y3,'k') 
+    % Gathering and Plotting Additional Time Responses At Different Magnitudes 
+    %opt1 = stepDataOptions('StepAmplitude',5);
+    opt2 = stepDataOptions('StepAmplitude',3);
+    opt3 = stepDataOptions('StepAmplitude',1); 
 
-xlabel('Time [s]') 
-ylabel('Speed [m/s]')
-h = legend('Measured','Model Fit','Step Magnitude = 3V','1V');
-rect = [-0.15, 0.3, 1, 1];
-set(h, ... 
-    'Position', rect, ... 
-    'FontSize', 12);  
+    %y1 = step(Gp,t_2,opt1);
+    y2 = step(Gp,t_2,opt2);
+    y3 = step(Gp,t_2,opt3);
 
-% Preserving the size of the image when saving 
-set(gcf,'InvertHardcopy','on');
-set(gcf,'PaperUnits', 'inches');
-papersize = get(gcf, 'PaperSize');
-left = (papersize(1)- width)/2;
-bottom = (papersize(2)- height)/2;
-myfiguresize = [left, bottom, width, height];
-set(gcf,'PaperPosition', myfiguresize);
+    hold on  
+    %plot(t_2,y1,'b')
+    plot(t_2,y2,'g')
+    plot(t_2,y3,'k') 
 
-print('SysIDPlot','-depsc2','-r600')
+    xlabel('Time [s]') 
+    ylabel('Speed [m/s]')
+    h = legend('Measured','Model Fit','Step Magnitude = 3V','1V');
+    rect = [-0.15, 0.3, 1, 1];
+    set(h, ... 
+        'Position', rect, ... 
+        'FontSize', 12);  
+
+    % Preserving the size of the image when saving 
+    set(gcf,'InvertHardcopy','on');
+    set(gcf,'PaperUnits', 'inches');
+    papersize = get(gcf, 'PaperSize');
+    left = (papersize(1)- width)/2;
+    bottom = (papersize(2)- height)/2;
+    myfiguresize = [left, bottom, width, height];
+    set(gcf,'PaperPosition', myfiguresize);
+
+    print('SysIDPlot','-depsc2','-r600') 
+end
